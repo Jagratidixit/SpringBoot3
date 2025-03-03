7ad3be68d72773279a78f238bc2d14eb2e40a0b9
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-04 04:18:30 +0530

    Make REST Call to show Hello Mark from BridgeLabz curl -X PUT localhost:8080/hello/put/Mark/?lastName=Taylor -w n

--- src/main/java/com/example/SpringBoot3/HelloController.java
+++ src/main/java/com/example/SpringBoot3/HelloController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/welcome")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
+
 
 
 

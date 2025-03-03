c25800ce0746f3c479c61aad86603da0cc7cfac8
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-04 03:51:42 +0530

    Make REST Call to show Hello Mark from BridgeLabz

--- src/main/java/com/example/SpringBoot3/HelloController.java
+++ src/main/java/com/example/SpringBoot3/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/welcome")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
+
